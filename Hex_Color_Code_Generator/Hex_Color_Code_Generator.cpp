553327d69ac8e0b11bb878a1763f4c74c4c9a755
Berkay Karadayı <c1511034@student.cankaya.edu.tr>
2024-06-05 21:17:22 +0300

    Proje dosyası ekle.

---
+++ Hex_Color_Code_Generator/Hex_Color_Code_Generator.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+
+#define bosluk endl
+
+using namespace std;
+
+
+void hex_code(int red, int green, int blue);
+
+int main()
+{
+	int r, g, b;
+	cout << "Please enter 3 integer: ";
+	cin >> r;
+	cin >> g;
+	cin >> b;
+
+	hex_code(r,g,b);
+
+
+	return 0;
+}
+
+void hex_code(int red, int green, int blue)
+{
+	const int numbers[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	const char alhpa[6] = {'a','b','c','d','e','f'};
+	int i = 0;
+	int div[20], reminder[20];
+
+	/*while (div[i] != 0) 
+	{
+		div[i] = red / 16;
+		reminder[i]=red % 16;
+		i++;
+		
+	}*/
+		
+
+	 
+
+	cout<<
+}
