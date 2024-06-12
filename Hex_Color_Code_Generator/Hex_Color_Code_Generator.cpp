f39e532a22cee2f6566f15518637e0edb1adf2d4
Berkay Karadayı <c1511034@student.cankaya.edu.tr>
2024-06-12 20:56:20 +0300

    fixing bugs.
    int array does not end with '\0' , so i tried to add it at the end of array :)) thats why errors occur i guess :)

--- Hex_Color_Code_Generator/Hex_Color_Code_Generator.cpp
+++ Hex_Color_Code_Generator/Hex_Color_Code_Generator.cpp
@@ -12,7 +12,7 @@ const int numbers[10] = { 0,1,2,3,4,5,6,7,8,9 };
 const char alhpa[6] = {'a','b','c','d','e','f'};
 
 void convert(int red, int green, int blue);
-void generate_hexcode(int division[],int remain[]);
+void generate_hexcode(int division[],int remain[], int rm_size);
 
 int main()
 {
@@ -32,11 +32,12 @@ void convert(int red, int green, int blue)
 {
 	
 	int j = 0;
-	int div[MAX] = {NULL}, remainder[MAX]={NULL};
+	int div[MAX], remainder[MAX];
 
 	int RGB[3] = { red,green,blue }; //to store our passed integer values.
 	int temp_div,temp_rem;
 
+	int s = 0, sayac = 0; int remain_size;
 	for (int i = 0; i < 3; i++)
 	{  
 		cout <<"---------------------------------" << " for i= " << i <<" And its value inside: "<<RGB[i] << "---------------------------------"<<bosluk;
@@ -47,15 +48,15 @@ void convert(int red, int green, int blue)
 			
 			
 
-			temp_div = (int)RGB[i]/16; //to get division op.
+			temp_div = RGB[i]/16; //to get division op.
 				temp_rem = RGB[i] % 16;
 			div[j] = temp_div;
 
-			  div[j + 1] = '\0'; //new added
+			  //div[j + 1] = '\0'; //new added
 
 			    remainder[j] = temp_rem;  // to get reminder op.
 
-			  remainder[j + 1] = '\0'; //new added, to see if it will fix heap corrpt. error.
+			  //remainder[j + 1] = '\0'; //new added, to see if it will fix heap corrpt. error.
 
 			RGB[i]=temp_div;
 
@@ -65,32 +66,44 @@ void convert(int red, int green, int blue)
 			cout << "rem[" << j << "]: " << remainder[j] << " "<<bosluk;
 			cout << bosluk << "----------------------------------------" << bosluk;
 		} while (div[j++] != 0 );
-	   
+	     
 		
 		cout <<bosluk<< "----------------------------------------"<<bosluk;
 	}	
 	
-	generate_hexcode(div, remainder);
+	 remain_size = sizeof(remainder) / sizeof(remainder[0]); // if we do like this, it will return fix sized of array, which is 50+1;
+	//		
+	//while (/*remainder[s] != '\0' &&*/ remainder[s] != 0) //To find array lenght of array
+	//{
+	//	sayac++;
+	//	s++;
+	//}
+	//remain_size = sayac+1;
+
+
+	generate_hexcode(div, remainder, remain_size);
 
 
 
 }
 
-void generate_hexcode(int division[], int remain[]) //  This function will generate hexa color code,
+void generate_hexcode(int division[], int remain[], int rm_size) //  This function will generate hexa color code,
 {
 	int i = 0, j=0,count=0, k=0,z=0 , div_count=0,z2=0;
 	int len;  int div_len; int temp_rem_len;
 
-	temp_rem_len = ((sizeof(remain) / sizeof(remain[0])) + 2);
+						//temp_rem_len = ((sizeof(remain) / sizeof(remain[0])) + 2);   /// ->>> does not give the right result??
 
-	int* temp_rem= new int  [temp_rem_len]; // +1 is for '/0'
+	int *temp_rem= new int [rm_size]; 
 
-	while (remain[z]!= '\0' && remain[z]>=0) //To find array lenght of array
-	{
-		count++;
-		z++;
-	}
-	 len = count-1;
+																	//while (/*remain[z]!= '\0' &&*/ remain[z]!=0) //To find array lenght of array
+																	//{
+																	//	count++;
+																	//	z++;
+																	//}
+	 
+	
+	len = count-1;
 
 	         
 	 //while (division[z2] != '\0' && division[z2] >= 0) //To find array lenght of array
@@ -104,7 +117,7 @@ void generate_hexcode(int division[], int remain[]) //  This function will gener
 	 //for (int i = 0; i < 3; i++) //// bu for olduğu için fazla dönüyor galiba????
 	 //{
 	 //}
-		while ((division[j] >= 0 && division[j] != '\0')  && (len >=0 && remain[len]!='\0'))
+		while ((division[j] != 0 /*&& division[j] != '\0'*/)  && (len >=0 && remain[len]!=0  /*'\0'*/))
 		{
 			//cout << "remain[" << j << "]: " << remain[j] << "  ";
 			  
@@ -114,6 +127,7 @@ void generate_hexcode(int division[], int remain[]) //  This function will gener
 			k++;
 			j++;
           
+			 //temp_rem[k] = '\0';
 
 		}
 		 ////
@@ -130,7 +144,7 @@ void generate_hexcode(int division[], int remain[]) //  This function will gener
 	// for closing tag }
 	  
 		
-	temp_rem[k] = '\0';
+  
 	//division[j] = '\0';*/
 	
 	
@@ -152,5 +166,5 @@ void generate_hexcode(int division[], int remain[]) //  This function will gener
 	}*/
 
 
-delete[] temp_rem;
+delete [] temp_rem;
 }
